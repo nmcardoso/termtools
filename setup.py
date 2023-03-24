a121a392e4e86dd1d34d08d915b15cfd381179bc
Natanael <natanael_magalhaes@msn.com>
2023-03-24 13:50:29 -0300

    include `tabulate` dependency

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
   name='termtools',
-  version='0.0.2',
+  version='0.0.3',
   description='Terminal Tools',
   author='Natanael',
   author_email='mail@natanael.net',
@@ -13,6 +13,7 @@ setup(
   },
   install_requires=[
     'wheel',
+    'tabulate',
   ],
   entry_points={
     'console_scripts': [
